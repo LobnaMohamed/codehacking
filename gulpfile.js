a2bf88786ee15325def3b5554ed3e16979e2b0b0
Lobna mahmoud <lobna@AMOC.COM>
2020-01-01 09:25:15 +0200

    admin master page setup

--- gulpfile.js
+++ gulpfile.js
@@ -17,10 +17,10 @@ elixir(function(mix) {
     .styles([
         'libs/blog-post.css',
         'libs/bootstrap.css',
-        'libs/bootstrap.min.css',
         'libs/font-awesome.css',
         'libs/metisMenu.css',
-        'libs/sb-admin-2.css'
+        'libs/sb-admin-2.css',
+        'libs/styles.css'
 
 
     ],'./public/css/libs.css')
@@ -31,6 +31,7 @@ elixir(function(mix) {
         'libs/jquery.js',
         'libs/bootstrap.js',
         'libs/metisMenu.js',
+        'libs/sb-admin-2.js',
         'libs/scripts.js'
 
 

c96cbcdec5a69a8d50e67edd7d16a4c7e964320f
LobnaMohamed <lobna92_19@hotmail.com>
2020-01-08 21:31:40 +0200

    finished till section 29

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -4,46 +4,23 @@
     <h1>categories</h1>
 
     <div class="col-sm-6">
-        {!! Form::model(['method'=>'post','action'=>'AdminCategoriesController@update'] )!!}
+        {!! Form::model($category,['method'=>'patch','action'=>['AdminCategoriesController@update',$category->id]] )!!}
             {{ csrf_field() }}
             <div class="form-group">
                 {!! Form::label('name', 'Category:') !!}
-                {!! Form::text('name', null, ['class'=>'form-control']) !!}
+                {!! Form::text('name', $category->name, ['class'=>'form-control']) !!}
 
             </div>
             <div class="form-group">
-                {!! Form::submit('save', ['class'=>' btn btn-primary']) !!}
+                {!! Form::submit('update category', ['class'=>' btn btn-primary']) !!}
+            </div>
+        {!! Form::close() !!}
+
+        {!! Form::open(['method'=>'delete','action'=>['AdminCategoriesController@destroy',$category->id]] )!!}
+            <div class="form-group">
+                {!! Form::submit('delete category', ['class'=>' btn btn-danger']) !!}
             </div>
         {!! Form::close() !!}
     </div>
-    <div class="col-sm-6">
-        @if ($categories)
-            <table class="table table-responsive table-stripped">
-                <thead>
-                    <th>id</th>
-                    <th>name</th>
-                    <th>created date</th>
 
-
-                </thead>
-                <tbody>
-                    
-                    @foreach ($categories as $category)
-                        <tr>
-                            <td>{{$category->id}}</td>
-                            <td>{{$category->name}}</td>
-                            <td>{{$category->created_at ? $category->created_at->diffForHumans() : "nodate"}}</td>
-
-                        </tr>
-
-                    @endforeach
-                    
-
-
-                </tbody>
-            </table>
-        @endif
-
-
-    </div>
 @endsection
